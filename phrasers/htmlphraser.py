9f0309f249cd1cf740af15113a52a2663ebcdb8f
Kyan <ykyankai@gmail.com>
2026-03-29 00:43:21 +0800

    Use walrus operator and bump Python to 3.8
    Refactor HtmlPhraser.to_html to use assignment expressions (:=) for phrases_div and row, adding a guard when the 'phrases' element is missing and simplifying row lookup/creation. Update pyproject.toml to require Python >=3.8 to reflect the use of the walrus operator and newer syntax.

--- phrasers/htmlphraser.py
+++ phrasers/htmlphraser.py
@@ -55,30 +55,29 @@ class HtmlPhraser(phraser.Phraser):
     def to_html(self):
         dirpath = os.path.dirname(os.path.abspath(__file__))
         soup = BeautifulSoup(open(os.path.join(dirpath, 'htmlphraser_tpl.html'), encoding='utf-8'), 'html.parser')
-        phrases_div = soup.html.body.find(id='phrases')
-        for itm in self.phrases:
-            row_attrs = {
-                'class': 'row',
-                'id': 'start-with-{}'.format(itm['shortcut'][0])
-            }
-            row = soup.find(attrs=row_attrs)
-            if not row:
-                row = soup.new_tag('div', attrs=row_attrs)
-                header = soup.new_tag('kbd', attrs={'class': 'col-12 text-center'})
-                header.string = itm['shortcut'][0]
-                row.append(header)
-                phrases_div.append(row)
-            col = soup.new_tag('div')
-            col['class'] = 'col-3 col-md-2'
-            phrs_item = soup.new_tag('div', attrs={'class': 'py-1 mt-1 text-center rounded border'})
-            phrs_shortcut = soup.new_tag('mark', attrs={'class': 'monofont'})
-            phrs_shortcut.string = itm['shortcut']
-            phrs_br = soup.new_tag('br')
-            phrs_phrase = soup.new_tag('span', attrs={'class': 'lead'})
-            phrs_phrase.string = itm['phrase']
-            phrs_item.append(phrs_shortcut)
-            phrs_item.append(phrs_br)
-            phrs_item.append(phrs_phrase)
-            col.append(phrs_item)
-            row.append(col)
+        if (phrases_div := soup.html.body.find(id='phrases')):
+            for itm in self.phrases:
+                row_attrs = {
+                    'class': 'row',
+                    'id': 'start-with-{}'.format(itm['shortcut'][0])
+                }
+                if not (row := soup.find(attrs=row_attrs)):
+                    row = soup.new_tag('div', attrs=row_attrs)
+                    header = soup.new_tag('kbd', attrs={'class': 'col-12 text-center'})
+                    header.string = itm['shortcut'][0]
+                    row.append(header)
+                    phrases_div.append(row)
+                col = soup.new_tag('div')
+                col['class'] = 'col-3 col-md-2'
+                phrs_item = soup.new_tag('div', attrs={'class': 'py-1 mt-1 text-center rounded border'})
+                phrs_shortcut = soup.new_tag('mark', attrs={'class': 'monofont'})
+                phrs_shortcut.string = itm['shortcut']
+                phrs_br = soup.new_tag('br')
+                phrs_phrase = soup.new_tag('span', attrs={'class': 'lead'})
+                phrs_phrase.string = itm['phrase']
+                phrs_item.append(phrs_shortcut)
+                phrs_item.append(phrs_br)
+                phrs_item.append(phrs_phrase)
+                col.append(phrs_item)
+                row.append(col)
         return str(soup)
